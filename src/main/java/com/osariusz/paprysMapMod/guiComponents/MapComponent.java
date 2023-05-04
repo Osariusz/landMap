49337cd82eed38f8322cc8c3e5b2ffd30c8a2931
Osariusz <osariusz@interia.pl>
2023-05-04 19:23:52 +0200

    comment purge

--- src/main/java/com/osariusz/paprysMapMod/guiComponents/MapComponent.java
+++ src/main/java/com/osariusz/paprysMapMod/guiComponents/MapComponent.java
@@ -63,23 +63,10 @@ public class MapComponent extends AbstractContainerScreen<MapMenu> {
                 this.texture.getPixels().setPixelRGBA(i, j, color);
             }
         }
-
         this.texture.upload();
     }
 
     public void render(PoseStack poseStack, int p_96053_, int p_96054_, float p_96055_) {
-
-        //this.fillGradient(poseStack, 0, 0, this.width, this.height, -12574688, -11530224);
-        //this.fillGradient(poseStack, 0, 0, this.width, this.height, -2, -11530224);
-        /*MultiBufferSource.BufferSource buffer = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
-        VertexConsumer consumer = buffer.getBuffer(RenderType.lines());
-        Matrix4f matrix4f = poseStack.last().pose();
-        TextureAtlasSprite textureAtlasSprite = new TextureAtlasSprite();*/
-
-            /*RenderSystem.setShaderTexture(0, VILLAGER_LOCATION);
-            RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-            blit(poseStack, this.leftPos + 83 + 99, this.topPos + 35, this.getBlitOffset(), 311.0F, 0.0F, 28, 21, 512, 256);*/
-
         updateTexture();
 
         int i = 0;
@@ -89,11 +76,6 @@ public class MapComponent extends AbstractContainerScreen<MapMenu> {
         MultiBufferSource.BufferSource multiBufferSource = MultiBufferSource.immediate(Tesselator.getInstance().getBuilder());
 
         VertexConsumer vertexconsumer = multiBufferSource.getBuffer(renderType);
-        /*vertexconsumer.vertex(matrix4f, 0.0F, 128.0F, -0.01F).color(255, 255, 255, 255).uv(0.0F, 1.0F).uv2(15728880).endVertex();
-        vertexconsumer.vertex(matrix4f, 128.0F, 128.0F, -0.01F).color(255, 255, 255, 255).uv(1.0F, 1.0F).uv2(15728880).endVertex();
-        vertexconsumer.vertex(matrix4f, 128.0F, 0.0F, -0.01F).color(255, 255, 255, 255).uv(1.0F, 0.0F).uv2(15728880).endVertex();
-        vertexconsumer.vertex(matrix4f, 0.0F, 0.0F, -0.01F).color(255, 255, 255, 255).uv(0.0F, 0.0F).uv2(15728880).endVertex();
-        int k = 0;*/
         vertexconsumer.vertex(matrix4f, 0.0F, (float)mapSegmentsY, -0.01F).color(255, 255, 255, 255).uv(0.0F, 1.0F).uv2(15728880).endVertex();
         vertexconsumer.vertex(matrix4f, (float)mapSegmentsX, (float)mapSegmentsY, -0.01F).color(255, 255, 255, 255).uv(1.0F, 1.0F).uv2(15728880).endVertex();
         vertexconsumer.vertex(matrix4f, (float)mapSegmentsX, 0.0F, -0.01F).color(255, 255, 255, 255).uv(1.0F, 0.0F).uv2(15728880).endVertex();
@@ -101,24 +83,6 @@ public class MapComponent extends AbstractContainerScreen<MapMenu> {
 
 
         multiBufferSource.endBatch();
-
-        //for(LogicalLine line : lines)
-        //{
-            //GuiComponent.fill(poseStack,line.getX1(),line.getY(),line.getX2()+1,line.getY()+1,line.getColor());
-
-            //this.hLine(poseStack, line.getX1(), line.getX2(), line. getY(), line.getColor());
-        //}
-
-        /*this.hLine(poseStack, 100, 300, 100, -2);
-        this.hLine(poseStack, 100, 300, 99, -2);
-        this.hLine(poseStack, 100, 300, 98, -2);*/
-
-        /*for(LogicalLine logicalLine : logicalMap.getLines()){
-            this.hLine(poseStack,logicalLine.getX1(),logicalLine.getX2(),logicalLine.getY(),logicalLine.getColor());
-        }*/
-
-        //drawCenteredString(poseStack, this.font, this.title, this.width / 2, 90, 16777215);
-        //drawCenteredString(poseStack, this.font, "blebleble", this.width / 2, 110, 16777215);
         super.render(poseStack, p_96053_, p_96054_, p_96055_);
     }
 
